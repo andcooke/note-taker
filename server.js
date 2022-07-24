b86f924bec288db0a4bc93c7ebeee417b36f08ef
A <andcooke@duck.com>
2022-07-24 11:26:39 -0500

    everything works

--- server.js
+++ server.js
@@ -20,13 +20,16 @@ app.get('/', (req, res) =>
 
 
 /******** ********  NOT WORKING!!!!  ******** ********/
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
 
 app.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/notes.html'))
   )
+
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
